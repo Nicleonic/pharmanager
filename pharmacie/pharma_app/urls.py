5e3dd726b9071ec6d8e7e1ec2d94f3b11a22188d
nicleonic <nicleonic2.0@gmail.com>
2024-03-18 01:03:31 +0200

    UX Upgrade

--- pharmacie/pharma_app/urls.py
+++ pharmacie/pharma_app/urls.py
@@ -6,6 +6,7 @@ import pharma_app.views as ph_views
 app_name = 'pharma_app'
 urlpatterns = [
     path('', ph_views.index, name='index'),
+    path('about', ph_views.about, name='about'),
     path('item/<int:item_id>/', ph_views.item, name='item_view')
 
 ]
--- pharmacie/pharma_app/views.py
+++ pharmacie/pharma_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Medicament
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -8,6 +9,16 @@ def index(request):
     return render(request, "pharma_app/index.html", {"title" : title, "medicaments" : medocs })
 
 def item(request, item_id):
-    medoc = Medicament.objects.get(id=item_id)
+    # try:
+    #     medoc = Medicament.objects.get(id=item_id)
+    # except:
+    #     raise Http404("Error 404")
+    # medoc = Medicament.objects.get_or_404(id=item_id)
+    medoc = get_object_or_404(Medicament, id=item_id)
     
-    return render(request, "pharma_app/item.html", {'title': medoc.nom, 'medoc': medoc})
+    return render(request, "pharma_app/item.html", {"title": medoc.nom, 'medoc': medoc})
+
+
+def about(request):
+    title = "About P<Manager>"
+    return render(request, "pharma_app/about.html",  {"title": title})
